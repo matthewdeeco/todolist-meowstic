54bde6e1c448aaae1fb5f4312d33b962c5d536c3
Matthew Co <matthewdeeco@gmail.com>
2014-12-05 13:48:49 +0800

    refactor check for if items are added to list

--- todo/functest/test_new_visitor.py
+++ todo/functest/test_new_visitor.py
@@ -5,28 +5,30 @@ from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(2)
-		self.address = 'http://localhost:8000'
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(2)
+        self.address = 'http://localhost:8000'
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get(self.address)
-		# Find the input box
-		inputbox = self.browser.find_element_by_id('new_item')
-		# Check title
-		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-		# Try inputting a new item
-		new_item_text = 'Submit CS 145 Machine Problem'
-		inputbox.send_keys(new_item_text)
-		inputbox.send_keys(Keys.ENTER)
-		# Check that the item appears in the list
-		table = self.browser.find_element_by_id('item_list')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(new_item_text, [row.text for row in rows])
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('item_list')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
-		self.fail('Finish the test!')
-		pass
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get(self.address)
+
+        new_item_texts = ['Submit CS 145 Machine Problem', 'Submit CS 180 Machine Problem']
+
+        # Input 2 items
+        for new_item_text in new_item_texts:
+            inputbox = self.browser.find_element_by_id('new_item')
+            inputbox.send_keys(new_item_text)
+            inputbox.send_keys(Keys.ENTER)
+
+        # Check that all items have been added
+        for new_item_text in new_item_texts:
+            self.check_for_row_in_list_table(new_item_text)
